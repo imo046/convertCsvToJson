cbd2f081233fa073e12b25136b69bbeee5a80bc5
Igor Molchanov <11870331+imo046@users.noreply.github.com>
2024-05-10 15:14:56 +0200

    init

---
+++ main/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+type Item struct {
+	Duration string `json:"Duration, sec"`
+	ID       string `json:"ID"`
+	Notes    string `json:"Notes"`
+	Text1    string `json:"Text_Session1"`
+	Text2    string `json:"Text_Session2"`
+	Text3    string `json:"Text_Session3"`
+}
+
+func standardizeSpaces(s string) string {
+	return strings.Join(strings.Fields(s), " ")
+}
+
+func convertToJsonArr(jsonData []byte) {
+	var myArr []Item
+	err := json.Unmarshal(jsonData, &myArr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, item := range myArr {
+		println(item.ID, item.Text1)
+	}
+
+}
+
+func run(pathToFile string, output string) {
+	// Open the CSV file
+	csvFile, err := os.Open(pathToFile)
+	if err != nil {
+		fmt.Println("Error opening CSV file:", err)
+		return
+	}
+	defer csvFile.Close()
+
+	// Read the CSV file
+	csvReader := csv.NewReader(csvFile)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		fmt.Println("Error reading CSV file:", err)
+		return
+	}
+
+	// Convert records to JSON
+	var jsonData []map[string]string
+	for _, row := range records {
+		data := make(map[string]string)
+		for i, col := range row {
+			// Assuming the first row contains the header
+			header := records[0][i]
+			data[header] = standardizeSpaces(col)
+		}
+		jsonData = append(jsonData, data)
+	}
+
+	// Convert JSON data to string
+	jsonDataString, err := json.Marshal(jsonData)
+	if err != nil {
+		fmt.Println("Error marshalling JSON:", err)
+		return
+	}
+
+	convertToJsonArr(jsonDataString)
+
+	// Write JSON data to file
+	//jsonFile, err := os.Create(output)
+	//if err != nil {
+	//	fmt.Println("Error creating JSON file:", err)
+	//	return
+	//}
+	//defer jsonFile.Close()
+	//
+	//_, err = jsonFile.Write(jsonDataString)
+	//if err != nil {
+	//	fmt.Println("Error writing JSON data to file:", err)
+	//	return
+	//}
+	//
+	//fmt.Println("Conversion completed. JSON data written to output.json")
+}
+
+func main() {
+	baseDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	println("base: " + baseDir)
+	filePath := fmt.Sprintf("%v/test.csv", baseDir)
+	outPath := fmt.Sprintf("%v/test.json", baseDir)
+	run(filePath, outPath)
+
+}
